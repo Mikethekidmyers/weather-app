01e5378b3904c5afa3534319892278ce51e6771b
MikeMyers <thomas.basso@live.no>
2021-11-11 22:36:10 +0100

    Changed error handler func to proper function

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ function App() {
 	const [weatherData, setWeatherData] = useState();
 	useEffect(() => {
 		if("geolocation" in navigator){
+			//The 1st param is a successfunc and the 2nd is the error handler
 			navigator.geolocation.getCurrentPosition((position) => {
 				const { latitude, longitude } = position.coords;
 				weatherDataFetcher(latitude, longitude, setWeatherData);
+			}, () => {
+				alert('Using this app requires browser location services to be enabled.')
 			});
-		} else {
-			//Do something if user has disabled location services
 		}
 	}, []);
 
