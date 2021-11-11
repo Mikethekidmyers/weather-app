b19d5ba150bb813c201dcc314e195f17ab53f741
MikeMyers <thomas.basso@live.no>
2021-11-11 22:14:49 +0100

    add modules to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import { Wrapper } from './components/Visuals';
 import Header from './components/Header';
 import Head from './components/Head';
+import weatherDataFetcher from './utils/fetcher';
+import ListModule from './components/ListModule';
 
 function App() {
+	const [weatherData, setWeatherData] = useState();
 	useEffect(() => {
-		if("geolocation" in navigator)
-			console.log(navigator);
+		if("geolocation" in navigator){
+			navigator.geolocation.getCurrentPosition((position) => {
+				const { latitude, longitude } = position.coords;
+				weatherDataFetcher(latitude, longitude, setWeatherData);
+			});
+		} else {
+			//Do something if user has disabled location services
+		}
 	}, []);
+
+	if(!weatherData) return null;
 	return (
-		<div className="App">
+		<HelmetProvider>
 			<Head />
 			<Header />
 			<Wrapper>
-
+				<ListModule {...weatherData} />
 			</Wrapper>
-		</div>
+		</HelmetProvider>
 	);
 }
 
