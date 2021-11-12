063d464af06fe8e3d3989190ef8a1c7677b156e8
MikeMyers <thomas.basso@live.no>
2021-11-12 02:39:05 +0100

    fetch user location via geocode module

--- src/App.js
+++ src/App.js
@@ -5,15 +5,31 @@ import Header from './components/Header';
 import Head from './components/Head';
 import weatherDataFetcher from './utils/fetcher';
 import ListModule from './components/ListModule';
+import Geocode from 'react-geocode';
+
+Geocode.setApiKey(process.env.REACT_APP_GEO_API_KEY);
+Geocode.setLocationType("ROOFTOP");
 
 function App() {
-	const [weatherData, setWeatherData] = useState();
+	const [weatherData, setWeatherData] = useState(null);
+	const [userLocation, setUserLocation] = useState(null);
 	useEffect(() => {
 		if("geolocation" in navigator){
 			//The 1st param is a successfunc and the 2nd is the error handler
 			navigator.geolocation.getCurrentPosition((position) => {
 				const { latitude, longitude } = position.coords;
+
 				weatherDataFetcher(latitude, longitude, setWeatherData);
+				
+				Geocode.fromLatLng(latitude, longitude).then(
+					(response) => {
+						const { long_name } = response.results[0].address_components.find(({ types }) => types.includes('postal_town'));
+						setUserLocation(long_name);
+					},
+					(error) => {
+						console.error(error);
+					}
+				);
 			}, () => {
 				alert('Using this app requires browser location services to be enabled.')
 			});
@@ -24,7 +40,7 @@ function App() {
 	return (
 		<HelmetProvider>
 			<Head />
-			<Header />
+			<Header location={userLocation} />
 			<Wrapper>
 				<ListModule {...weatherData} />
 			</Wrapper>
